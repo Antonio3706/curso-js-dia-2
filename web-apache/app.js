e8ad229a065f6ed4fe2bdc99084d2edb0c5780f5
Antonio3706 <antoniocesargavirap@gmail.com>
2026-05-22 11:11:03 +0200

    Añadiendo los .js

---
+++ web-apache/app.js
@@ -0,0 +1,6 @@
+import { tecnologias } from './data/tecnologias.js';
+import { crearTarjetaTecnologia } from './utils/dom.js';
+const contenedor = document.querySelector('#contenedor-tecnologias');
+for (const tecnologia of tecnologias) {
+const tarjeta = crearTarjetaTecnologia(tecnologia);
+}
