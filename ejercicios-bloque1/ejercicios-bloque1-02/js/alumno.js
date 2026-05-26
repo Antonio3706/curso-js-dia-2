8066d73fc49eebc206bd9bf85fd5387dd3147628
Antonio3706 <antoniocesargavirap@gmail.com>
2026-05-26 13:50:17 +0200

    Adiciones

---
+++ ejercicios-bloque1/ejercicios-bloque1-02/js/alumno.js
@@ -0,0 +1,13 @@
+// Sin objeto (difícil de manejar):
+const nombre = 'Ana';
+const edad = 22;
+const curso = 'JavaScript avanzado';
+// Con objeto (todo junto y relacionado):
+const alumno = {
+    nombre: 'Ana',
+    edad: 22,
+    curso: 'JavaScript avanzado',
+    activo: true
+};
+// Acceder a una propiedad:
+console.log(alumno.nombre); // 'Ana'
