55d6d0e07fd02bb8f00f821c111b2a692ed81f3a
Antonio3706 <antoniocesargavirap@gmail.com>
2026-05-26 14:01:28 +0200

    Ejercicio 3

---
+++ ejercicios-bloque1/ejercicios-bloque1-03/js/productos.js
@@ -0,0 +1,11 @@
+// Array de objetos: todos tienen la misma forma
+const productos = [
+    { nombre: 'Teclado', precio: 35, categoria: 'Informática', disponible:
+    true },
+    { nombre: 'Monitor', precio: 249, categoria: 'Informática', disponible:
+    true },
+    { nombre: 'Alfombrilla', precio: 12, categoria: 'Accesorios', disponible:
+    false },
+];
+// Acceder al segundo elemento:
+console.log(productos[1].nombre); // 'Monitor'
