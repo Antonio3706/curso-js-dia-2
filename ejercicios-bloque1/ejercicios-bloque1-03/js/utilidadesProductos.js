a6143efab5ac202c292a6b88d6b9ccc804b405fb
Antonio3706 <antoniocesargavirap@gmail.com>
2026-05-27 14:03:26 +0200

    Update utilidadesProductos.js

--- ejercicios-bloque1/ejercicios-bloque1-03/js/utilidadesProductos.js
+++ ejercicios-bloque1/ejercicios-bloque1-03/js/utilidadesProductos.js
@@ -24,5 +24,14 @@ export function obtenerNombres(productos){
 }
 
 export function crearTextoResumen(productos){
-    
+    let nombres="";
+    const total=0;
+    let contador=0;
+    while(contador<productos.length){
+        //Hacerlo como un array
+        nombres=(nombres+productos[contador].nombre+", ");
+        total=total+producto[contador].precio;
+        contador++;
+    }
+    return ("Los productos son: "+nombres+" y en total valen "+total+"€.");
 }
