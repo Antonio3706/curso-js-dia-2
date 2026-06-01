e427754b3b610be346800e32de46b8d0201a991e
Antonio3706 <antoniocesargavirap@gmail.com>
2026-06-01 08:52:56 +0200

    Cambios en el proyecto

---
+++ Proyecto/js/productos.js
@@ -0,0 +1,10 @@
+export const productos=[
+    {nombre: 'Silla', precio: 0, descripcion: '', categoria: ''},
+    {nombre: 'Mesa', precio: 0, descripcion: '', categoria: ''},
+    {nombre: 'Escritorio', precio: 0, descripcion: '', categoria: ''},
+    {nombre: 'Armario', precio: 0, descripcion: '', categoria: ''},
+    {nombre: 'Inodoro', precio: 0, descripcion: '', categoria: ''},
+    {nombre: 'Encimera', precio: 0, descripcion: '', categoria: ''},
+    {nombre: 'Estanteria', precio: 0, descripcion: '', categoria: ''},
+    {nombre: 'Perchero', precio: 0, descripcion: '', categoria: ''},
+];
