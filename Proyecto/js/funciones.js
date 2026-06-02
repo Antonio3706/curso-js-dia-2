b3dc5cc8311fe45661c234ffd0fc66cd0e7df550
Antonio3706 <antoniocesargavirap@gmail.com>
2026-06-02 12:13:05 +0200

    Update funciones.js

--- Proyecto/js/funciones.js
+++ Proyecto/js/funciones.js
@@ -1,9 +1,65 @@
-import { silla } from "./productos.js";
+import { silla, mesa, escritorio, estanteria, encimera, armario, inodoro, perchero} from "./productos.js";
 
-export function ponerNombre(silla){
-    const botonSilla = document.querySelectorAll(".boton");
+export function ponerNombreSilla(silla){
+    const boton = document.querySelectorAll(".boton");
         
-    for (let i=0; i<botonSilla.length; i++){
-        botonSilla[i].textContent=silla[i].nombre;
+    for (let i=0; i<boton.length; i++){
+        boton[i].textContent=silla[i].nombre;
+    }
+}
+
+export function ponerNombreMesa(mesa){
+    const boton = document.querySelectorAll(".boton");
+        
+    for (let i=0; i<boton.length; i++){
+        boton[i].textContent=mesa[i].nombre;
+    }
+}
+
+export function ponerNombreEscritorio(escritorio){
+    const boton = document.querySelectorAll(".boton");
+        
+    for (let i=0; i<boton.length; i++){
+        boton[i].textContent=escritorio[i].nombre;
+    }
+}
+
+export function ponerNombreEstanteria(estanteria){
+    const boton = document.querySelectorAll(".boton");
+        
+    for (let i=0; i<boton.length; i++){
+        boton[i].textContent=estanteria[i].nombre;
+    }
+}
+
+export function ponerNombreEncimera(encimera){
+    const boton = document.querySelectorAll(".boton");
+        
+    for (let i=0; i<boton.length; i++){
+        boton[i].textContent=encimera[i].nombre;
+    }
+}
+
+export function ponerNombreArmario(armario){
+    const boton = document.querySelectorAll(".boton");
+        
+    for (let i=0; i<boton.length; i++){
+        boton[i].textContent=armario[i].nombre;
+    }
+}
+
+export function ponerNombreInodoro(inodoro){
+    const boton = document.querySelectorAll(".boton");
+        
+    for (let i=0; i<boton.length; i++){
+        boton[i].textContent=inodoro[i].nombre;
+    }
+}
+
+export function ponerNombrePerchero(perchero){
+    const boton = document.querySelectorAll(".boton");
+        
+    for (let i=0; i<boton.length; i++){
+        boton[i].textContent=perchero[i].nombre;
     }
 }
