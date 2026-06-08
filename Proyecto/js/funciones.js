43c6accea660a3608db219b71e651d9cb3a0422a
Antonio3706 <antoniocesargavirap@gmail.com>
2026-06-08 10:48:24 +0200

    Update funciones.js

--- Proyecto/js/funciones.js
+++ Proyecto/js/funciones.js
@@ -10,15 +10,19 @@ export function ponerNombre(productos){
 
 export function ventanaPro(productos){
     const botones=document.querySelectorAll(".boton");
+    const producto=JSON.parse(
+        localStorage.getItem("prodSelec")
+    );
     for (let i=0; i<botones.length; i++){
         botones[i].textContent=productos[i].nombre;
         botones[i].addEventListener("click",()=>{
             localStorage.setItem("prodSelec", JSON.stringify(productos[i]));
             window.location.href="descripciones.html";
-        });        
+        });
+        document.getElementById("titulo").textContent=producto.nombre;        
     }
 }
 
 export function ponerInformacion(){
-    
+
 }
