f99773ff9acacfd6cb01b7e61651de42f0f5d470
Antonio3706 <antoniocesargavirap@gmail.com>
2026-06-08 11:38:26 +0200

    Update prueba.js

--- Proyecto/js/prueba.js
+++ Proyecto/js/prueba.js
@@ -3,18 +3,20 @@
 
 import { silla, mesa, escritorio, estanteria, encimera, armario, inodoro, perchero} from "./productos.js";
     
-
-function ventanaPro(productos){
     const botones=document.querySelectorAll(".boton");
-    const producto=JSON.parse(
-        localStorage.getItem("prodSelec")
-    );
     for (let i=0; i<botones.length; i++){
         botones[i].textContent=productos[i].nombre;
         botones[i].addEventListener("click",()=>{
             localStorage.setItem("prodSelec", JSON.stringify(productos[i]));
             window.location.href="descripciones.html";
         });
-        console.log(document.getElementById("nombrePro").textContent=producto.nombre);        
     }
-};
+
+function ponerInformacion(){
+console.log(localStorage.getItem("prodSelec"));
+
+    const producto=JSON.parse(
+        localStorage.getItem("prodSelec")
+    );
+    console.log(producto);
+}
