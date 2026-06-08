18dd3ffecb19115d65a552d3128ce9bc8f90b02c
Antonio3706 <antoniocesargavirap@gmail.com>
2026-06-08 13:02:02 +0200

    Update main.js

--- Proyecto/js/main.js
+++ Proyecto/js/main.js
@@ -1,5 +1,5 @@
 import { silla, mesa, escritorio, armario, inodoro, encimera, estanteria, perchero } from "./productos.js";
-import { ponerNombre, ventanaPro, ponerInformacion } from "./funciones.js";
+import { cargarProducto, ponerInformacion } from "./funciones.js";
 
 const control1=document.getElementById("silla");
 const control2=document.getElementById("armario");
@@ -12,60 +12,51 @@ const control8=document.getElementById("perchero");
 
 if(document.getElementById("silla")){
     control1.addEventListener('click', () => {
-        ponerNombre(silla);
+        cargarProducto(silla);
     });
 }
 
 if(document.getElementById("armario")){
     control2.addEventListener('click', () => {
-        ponerNombre(armario);
+        cargarProducto(armario);
     });
 }
 
 if(document.getElementById("encimera")){
     control3.addEventListener('click', () => {
-        ponerNombre(encimera);
+        cargarProducto(encimera);
     });
 }
 
 if(document.getElementById("escritorio")){
     control4.addEventListener('click', () => {
-        ponerNombre(escritorio);
+        cargarProducto(escritorio);
     });
 }
 
 if(document.getElementById("estanteria")){
     control5.addEventListener('click', () => {
-        ponerNombre(estanteria);
+        cargarProducto(estanteria);
     });
 }
 
 if(document.getElementById("inodoro")){
     control6.addEventListener('click', () => {
-        ponerNombre(inodoro);
+        cargarProducto(inodoro);
     });
 }
 
 if(document.getElementById("mesa")){
     control7.addEventListener('click', () => {
-        ponerNombre(mesa);
+        cargarProducto(mesa);
     });
 }
 
 if(document.getElementById("perchero")){
     control8.addEventListener('click', () => {
-        ponerNombre(perchero);
+        cargarProducto(perchero);
     });
 }
-
-ventanaPro(silla);
-ventanaPro(mesa);
-ventanaPro(inodoro);
-ventanaPro(encimera);
-ventanaPro(escritorio);
-ventanaPro(estanteria);
-ventanaPro(perchero);
-ventanaPro(armario);
 
 window.addEventListener("DOMContentLoaded", () =>{
     ponerInformacion();
