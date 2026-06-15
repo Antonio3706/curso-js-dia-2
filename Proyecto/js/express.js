c3f5982ff2aa6d0ac58f5f585e7beca9eaf25e44
Antonio3706 <antoniocesargavirap@gmail.com>
2026-06-15 13:13:18 +0200

    Update express.js

--- Proyecto/js/express.js
+++ Proyecto/js/express.js
@@ -1,5 +1,5 @@
 import express from "express";
 const app = express();
 app.listen(5500, () => {
-console.log("Servidor iniciado en http://localhost:3000");
+console.log("Servidor iniciado en http://localhost:5500");
 });
