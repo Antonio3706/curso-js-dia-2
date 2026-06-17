c469519ec4f50e35982767bcac642f31685c6373
Antonio3706 <antoniocesargavirap@gmail.com>
2026-06-17 10:59:18 +0200

    j

--- Proyecto/src/app.js
+++ Proyecto/src/app.js
@@ -2,11 +2,11 @@
 
 const express=require('express');
 const app=express();
+const path = require('path');
 
 app.set('view engine', 'ejs');
 
-
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000);
 
