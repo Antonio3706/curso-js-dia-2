005a68925f97acc22d619c2eabcdfdbdb8d4a2b6
Antonio3706 <antoniocesargavirap@gmail.com>
2026-06-19 11:44:43 +0200

    Delete p.js

--- Proyecto/public/js/p.js
+++
@@ -1,7 +0,0 @@
-import { silla, mesa, escritorio, armario, inodoro, encimera, estanteria, perchero, prod } from "./productos.js";
-import { cargarProducto, ponerInformacion, mostrarOpcionesBusqueda, alternarOpcionesBusqueda, botonesCategoria, botonesMarca } from "./funciones.js";
-
-
-console.log(
-    prod.filter(p => !p.categoria || p.categoria.trim() === "")
-);
