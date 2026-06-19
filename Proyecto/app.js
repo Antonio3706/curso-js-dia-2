c4f1597fdad819aceb99b1c03f43d34f888b5497
Antonio3706 <antoniocesargavirap@gmail.com>
2026-06-19 19:16:54 +0200

    Update app.js

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -13,7 +13,7 @@ const __dirname = path.dirname(__filename);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "src/views"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "src")));
 
 // Rutas
 app.get("/", (req, res) => {
