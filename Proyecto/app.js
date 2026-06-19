054f598e15a6f9f4476e95eac1f06eabbeda8166
Antonio3706 <antoniocesargavirap@gmail.com>
2026-06-19 17:23:39 +0200

    Update app.js

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -7,7 +7,7 @@ const path = require('path');
 app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, 'src/views'));
-
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
