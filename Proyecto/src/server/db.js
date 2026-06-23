bf964b077b60e7a2e4d6fc86a18e06aef56cbb62
Antonio3706 <antoniocesargavirap@gmail.com>
2026-06-23 12:11:58 +0200

    Update db.js

--- Proyecto/src/server/db.js
+++ Proyecto/src/server/db.js
@@ -3,6 +3,7 @@ import mysql from "mysql2/promise";
 console.log("Creando conexion...");
 export const connection = await mysql.createConnection({
     host: "localhost",
+    port:3306,
     user: "root",
     password: "root",
     database: "wood_trend"
